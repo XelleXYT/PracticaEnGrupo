8e5d1138734ea36647022e48ee32504afb224749
Junior <Junior@LAPTOP-3HBQ8ARS>
2018-01-19 19:11:46 +0100

    Terminado el codigo de CodinBat de JoseA

---
+++ src/codigo/ClaseJose.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+/**
+ *
+ * @author Junior
+ */
+public class ClaseJose {
+
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+
+        if (isWeekend) {
+            return (cigars >= 40);
+        }
+        {
+
+        }
+        return cigars >= 40 && cigars <= 60;
+    }
+    
+    
+
+}
